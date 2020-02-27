9eabc5ab187178261235b7153f1b5d062e2e22fd
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-02-26 18:44:41 -0800

    Set up Routes in App.js.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import {Route} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home/index';
+import About from './components/About/index';
+import Projects from './components/Projects/index';
+import Skills from './components/Skills/index';
+import Contact from './components/Contact/index';
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Switch>
+				<Route path='/about'>
+					<About />
+				</Route>
+				<Route path='/projects'>
+					<Projects />
+				</Route>
+				<Route path='/skills'>
+					<Skills />
+				</Route>
+				<Route path='/contact'>
+					<Contact />
+				</Route>
+				<Route path='/'>
+					<Home />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
