bbf34ab82ad726d26d7b17716164d383d0c4ba99
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-08-29 09:44:01 -0700

    fix global app responsiveness with styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Box, useColorMode } from '@chakra-ui/core';
 
 import Home from './components/Home';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Contact from './components/About';
 
 import './App.css';
-import NavBar from './NavBar';
+import NavBar from './components/NavBar';
+
+import Footer from './components/Footer';
 
 function App() {
+	const { colorMode } = useColorMode();
 	return (
 		<div className='App'>
 			<NavBar />
-			<Switch>
-				<Route path='/projects'>
-					<Projects />
-				</Route>
-				<Route path='/contact'>
-					<Contact />
-				</Route>
-				<Route path='/'>
-					<Home />
-				</Route>
-			</Switch>
+			<Box
+				flex='1 0 auto'
+				p='10% 7%'
+				background={
+					colorMode === 'light'
+						? 'linear-gradient(211deg, rgba(255,255,255,1) 0%, rgba(227,227,227,1) 100%)'
+						: 'linear-gradient(211deg, rgba(0,0,0,1) 0%, rgba(71,71,71,1) 100%)'
+				}
+			>
+				
+				{/* <Box
+				p='0 2%'
+				textAlign='center'
+				> */}
+					<Switch>
+						<Route path='/projects'>
+							<Projects />
+						</Route>
+						<Route path='/about'>
+							<Contact />
+						</Route>
+						<Route path='/'>
+							<Home />
+						</Route>
+					</Switch>
+				{/* </Box> */}
+			</Box>
+
+			<Footer />
 		</div>
 	);
 }
