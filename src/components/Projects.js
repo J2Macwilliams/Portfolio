05314dcf6309e548edf441dbc4cbad34347f8c7b
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-08-29 09:45:30 -0700

    add gsap fn to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,40 @@
-import React from 'react'
-
+import React, {useRef} from 'react';
+import { useIntersection } from 'react-use';
+// import { Flex } from '@chakra-ui/core';
+import gsap from 'gsap';
 const Projects = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	
+	const sectionRef = useRef(null);
+	const intersection = useIntersection(sectionRef, {
+		root: null,
+		rootMargin: '400px',
+		threshold: 1,
+	});
 
-export default Projects
+	const fadeIn = element => {
+		gsap.to(element, 1, {
+			opacity: 1,
+			y: -60,
+			ease: 'power4.out',
+			stagger: {
+				amount: 0.3,
+			},
+		});
+	};
+
+	const fadeOut = element => {
+		gsap.to(element, 1, {
+			opacity: 0,
+			y: -20,
+			ease: 'power4.out',
+		});
+	};
+	return (
+		<>
+		
+		</>
+	);
+};
+
+export default Projects;
 
