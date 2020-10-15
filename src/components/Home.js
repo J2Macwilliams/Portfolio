9fcae986728624c4c34bda48ab511d9d9e9476b3
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-10-15 16:18:34 -0700

    move name and title animation to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
-import { Text, Heading, Box, Flex } from '@chakra-ui/core';
+import { Text,  Box, Flex, Heading } from '@chakra-ui/core';
 
 import gsap from 'gsap';
 
 const Home = () => {
+	function intro() {
+		var t1 = gsap
+			.timeline()
+			.from('.first', {
+				duration: 1.5,
+				opacity: 0,
+				ease: 'bounce.out',
+				y: 400,
+			})
+			.from(
+				'.middle',
+				{
+					duration: 1.5,
+					opacity: 0,
+					ease: 'bounce.out',
+					y: 400,
+				},
+				'-=1'
+			)
+			.from(
+				'.last',
+				{
+					duration: 1.25,
+					opacity: 0,
+					ease: 'bounce.out',
+					y: 400,
+				},
+				'-=1.25'
+			);
+		return t1;
+	}
 	function Full() {
 		var t2 = gsap.timeline().from('.full', {
 			duration: 2,
@@ -17,19 +48,44 @@ const Home = () => {
 
 	useEffect(() => {
 		var master = gsap.timeline();
-		master.add(Full(), '+=2.4');
+		master.add(intro()).add(Full());
 	}, []);
 
 	return (
 		<>
 			<Box>
-				<Flex justify='center'>
+			<Flex direction='column' align='center'>
+						<Flex>
+							<Heading
+								m='1%'
+								fontSize={['2xl', '4xl', '5xl', '5xl']}
+								className='first'
+							>
+								Jeremy
+							</Heading>
+							<Heading
+								m='1%'
+								fontSize={['2xl', '4xl', '5xl', '5xl']}
+								className='middle'
+							>
+								J.
+							</Heading>
+
+							<Heading
+								m='1%'
+								fontSize={['2xl', '4xl', '5xl', '5xl']}
+								className='last'
+							>
+								McWilliams
+							</Heading>
+						</Flex>
+					
 					<Text
 						fontSize={['xl', '2xl', '3xl', '4xl']}
 						marginLeft='2%'
 						className='full'
 					>
-						Full Stack Developer
+						Full-Stack Engineer
 					</Text>
 				</Flex>
 			</Box>
